61692495901c335750a2e3f60d315cf29022d309
killux = Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-07-23 10:52:04 +0100

    jedliks

--- jedliks-toy-car/src/main/java/JedliksToyCar.java
+++ jedliks-toy-car/src/main/java/JedliksToyCar.java
@@ -1,17 +1,27 @@
 public class JedliksToyCar {
+
+    private int distance;
+    private int battery = 100;;
+
     public static JedliksToyCar buy() {
-        throw new UnsupportedOperationException("Please implement the (static) JedliksToyCar.buy()  method");
+        return new JedliksToyCar();
     }
 
     public String distanceDisplay() {
-        throw new UnsupportedOperationException("Please implement the JedliksToyCar.distanceDisplay()  method");
+        return String.format("Driven %d meters", distance);
     }
 
     public String batteryDisplay() {
-        throw new UnsupportedOperationException("Please implement the JedliksToyCar.batteryDisplay()  method");
+        if (battery == 0) {
+            return "Battery empty";
+        }
+        return String.format("Battery at %d%%", battery);
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the JedliksToyCar.drive()  method");
+        if (battery != 0) {
+            distance += 20;
+            battery--;
+        }
     }
 }
