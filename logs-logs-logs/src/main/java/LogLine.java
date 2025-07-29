a27f77e38f14efb2f85bf10668bad909c14aed02
Mustapha Boutoub | killux <mustaphaboutoubdev@gmail.com>
2025-07-29 16:27:36 +0100

    logs

---
+++ logs-logs-logs/src/main/java/LogLine.java
@@ -0,0 +1,26 @@
+public class LogLine {
+
+    private LogLevel log;
+    private String msg;
+
+    public LogLine(String logLine) {
+        this.log = switch (logLine.substring(1, 4)) {
+            case "TRC" -> LogLevel.TRACE;
+            case "DBG" -> LogLevel.DEBUG;
+            case "INF" -> LogLevel.INFO;
+            case "WRN" -> LogLevel.WARNING;
+            case "ERR" -> LogLevel.ERROR;
+            case "FTL" -> LogLevel.FATAL;
+            default -> LogLevel.UNKNOWN;
+        };
+        this.msg = logLine.substring(7);
+    }
+
+    public LogLevel getLogLevel() {
+        return log;
+    }
+
+    public String getOutputForShortLog() {
+        return "" + this.log.nbr + ":" + this.msg;
+    }
+}
